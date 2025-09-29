939e1b873512b1989372b8508c40e62cbf893e95
Serhii Sidoruk <serhii.sidoruk@outlook.com>
2025-09-29 04:55:44 +0300

    feat(nav-main): add translations for platform label in NavMain component

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -2,6 +2,7 @@
 
 import type { FC } from "react";
 import { ChevronRight } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 import {
   SidebarGroup,
@@ -23,9 +24,10 @@ import { Link } from "@/i18n/routing";
 import type { NavMainItem } from "@/shared/types/nav-main";
 
 export const NavMain: FC<{ items: Array<NavMainItem> }> = ({ items }) => {
+  const t = useTranslations("NavMain");
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarGroupLabel>{t("platform")}</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item) => (
           <Collapsible key={item.title} asChild defaultOpen={item.isActive}>
